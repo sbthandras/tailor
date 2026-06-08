a8e7cb09466814a01f33ad85b43414711a1702a7
Tamas Stirling <stirling.tamas@brc.hu>
2026-06-08 13:13:43 +0200

    Update default value for 'may_start'

--- R/find_adapter.R
+++ R/find_adapter.R
@@ -4,31 +4,31 @@
 #' alignment and detects conserved N-terminal regions, optinally merging
 #' neighboring conserved regions.
 #' @param bps data.frame; a data frame of breakpoints
-#' @param min_pident numeric; the lowest accepted percentage identity for a 
+#' @param min_pident numeric; the lowest accepted percentage identity for a
 #' region to be considered a "conserved" region.
-#' @param max_start integer; the maximum accepted starting position for the 
+#' @param max_start integer; the maximum accepted starting position for the
 #' first conserved region.
-#' @param merge_beginning logical; whether to merge the positions before the 
+#' @param merge_beginning logical; whether to merge the positions before the
 #' first conserved region. See Details for more information.
-#' @param merge_conserved logical; whether neighboring conserved regions should 
+#' @param merge_conserved logical; whether neighboring conserved regions should
 #' be merged. See Details for more information.
 #' @return A data frame of class "adapter".
-#' @details The function returns a single row for each pairwise comparison. If 
-#' no conserved N-terminal regions are identified, the function returns a row 
-#' with NA values for the start, end, mean_score, and pident columns. If a 
-#' single conserved N_terminal region is identified, the function returns the 
-#' start and end positions, mean score, and percentage identity of this region. 
+#' @details The function returns a single row for each pairwise comparison. If
+#' no conserved N-terminal regions are identified, the function returns a row
+#' with NA values for the start, end, mean_score, and pident columns. If a
+#' single conserved N_terminal region is identified, the function returns the
+#' start and end positions, mean score, and percentage identity of this region.
 #' If multiple conserved N-terminal regions are identified, behaviour depends on
-#' the values of `merge_conserved` and `merge_beginning`. 
-#' @details If `merge_conserved` is TRUE, neighboring conserved regions will be 
-#' merged and the mean score and percentage identity of the merged region will 
+#' the values of `merge_conserved` and `merge_beginning`.
+#' @details If `merge_conserved` is TRUE, neighboring conserved regions will be
+#' merged and the mean score and percentage identity of the merged region will
 #' be recalculated. If  `merge_conserved` is FALSE, the function will only keep
 #' the first conserved region.
-#' @details If `merge_beginning` is TRUE, and there is a conserved region 
-#' (merged or unmerged) which starts within `max_start`, but not at position 1, 
-#' then the function will also merge the positions before this conserved region 
+#' @details If `merge_beginning` is TRUE, and there is a conserved region
+#' (merged or unmerged) which starts within `max_start`, but not at position 1,
+#' then the function will also merge the positions before this conserved region
 #' and recalculate the mean score and percentage identity of the merged region.
-#' If the percentage identity of the merged region is below `min_pident`, the 
+#' If the percentage identity of the merged region is below `min_pident`, the
 #' function will drop the merged region and return NA values for the start, end,
 #' mean_score, and pident columns.
 #' @examples
@@ -40,7 +40,7 @@
 find_adapter <- function(
     bps,
     min_pident = 0.4,
-    max_start = 15,
+    max_start = 10,
     merge_beginning = TRUE,
     merge_conserved = TRUE
   ) {
@@ -72,7 +72,7 @@ find_adapter <- function(
   } else if (consreg$start != 1 && merge_beginning) {
     adapter <- merge_regions(
       dplyr::bind_rows(
-        bps[which(bps$end < consreg$start), ], 
+        bps[which(bps$end < consreg$start), ],
         consreg
       )
     )
